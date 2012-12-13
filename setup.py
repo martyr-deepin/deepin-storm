7e3d39139b0a44f4acab8228bc7c8471b0234c9c
Andy Stewart <lazycat.manatee@gmail.com>
2012-12-13 15:28:01 +0800

    Add setup.py to install deepin storm.

---
+++ setup.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+
+from setuptools import setup
+
+setup(name='deepin_storm',
+      version='1.0',
+      description='Python download library for Linux Deepin.',
+      long_description ="""Python download library for Linux Deepin.""",
+      author='Wang Yong',
+      author_email='wangyong@linuxdeepin.com',
+      license='GPL-3',
+      url="https://github.com/linuxdeepin/deepin-storm",
+      download_url="git://github.com/linuxdeepin/deepin-storm.git",
+      platforms = ['Linux'],
+      packages = ['deepin_storm'],
+      )
+
